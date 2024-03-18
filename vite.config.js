f9a1cd1dd2e4c585a7dc17b36ea304cdad95f507
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-03-18 12:28:11 +0200

    vite+scss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import vitePluginSass from "vite-plugin-sass";
 
 export default defineConfig({
-  plugins: [react(), vitePluginSass()],
+  plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
